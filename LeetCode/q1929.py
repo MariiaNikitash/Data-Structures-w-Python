296118378411cef75302425cf8b54c6ac57dba24
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2023-09-17 22:19:05 -0700

    array concantination q solved

---
+++ LeetCode/q1929.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        #create another array
+        ans = []
+        #loop 2 times
+        for i in (2):
+            #loop in nums
+            for n in nums:
+                ans.append(n)
+        return ans
+
