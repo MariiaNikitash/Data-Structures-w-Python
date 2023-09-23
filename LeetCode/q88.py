af6e45d5445f973231a2f7d45d7d48fa50f23b68
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2023-09-22 20:01:31 -0700

    started solving using while to merge in reverse order

--- LeetCode/q88.py
+++ LeetCode/q88.py
@@ -1,6 +1,19 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        # Last index of nums1 
+        last = m + n - 1
+
+        # Merge in reverse order
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[last] = nums1[m - 1]
+                # don't forget to decrement m
+                m -= 1
+            else:
+                # ??
+                nums1[last] < nums2[n - 1]:
+                
+
+            
 
 
-
-        
