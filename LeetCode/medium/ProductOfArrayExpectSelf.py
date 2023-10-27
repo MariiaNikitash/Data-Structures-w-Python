e7d20affbdb0ee38aa0ff6408c63a7f25e270dd7
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2023-10-26 18:19:26 -0700

    explained the solution better

--- LeetCode/medium/ProductOfArrayExpectSelf.py
+++ LeetCode/medium/ProductOfArrayExpectSelf.py
@@ -1,22 +1,28 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
         # make a list of the size of nums
-        answer = [1]* len(nums)
+        answer = [1] * len(nums)
 
         # make a list of the size of nums 
         # for the indexes before i
         left = [1] * len(nums)
-        for i in range(1, len(nums)):
-            # 
-            left[i] = left[i-1] * nums[i-1]
+        #start at index 1 because thers no elems before index 0
+        for index in range(1, len(nums)):
+            # so index = 
+            left[index] = left[index-1] * nums[index-1]
 
+        # same here do list for right = [1,1,1,1]
         right = [1]* len(nums)
-        for j in reversed(range(len(nums)-1)):
-            right[j] = right[j+1] * nums[j+1]
 
+        # since we need multiply all indexes to the right of index 
+        # we do reverse loop and start at the second to last index
+        for index in reversed(range(len(nums)-1)):
+            right[index] = right[index+1] * nums[index+1]
 
-        for k in range(len(answer)):
-            answer[k] = left[k] * right[k]
+        # loop to multiply index of left with index of right
+        for index in range(len(answer)):
+            answer[index] = left[index] * right[index]
         return answer
+    
+
+
