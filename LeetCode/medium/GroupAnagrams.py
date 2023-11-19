907a8fab885d3c81ffe2f4b417de142dfd342c90
MariiaNikitash <nikitash.mariia2004@csu.fullerton.edu>
2023-11-18 20:55:58 -0800

    solved by using sorted tuples, dict and list

--- LeetCode/medium/GroupAnagrams.py
+++ LeetCode/medium/GroupAnagrams.py
@@ -1,18 +1,18 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dic = [] # will be used to store anagrams
-        ans = {} # will eventually contain grouped anagrams
+        dic = {} # will be used to store anagrams
+        ans = [] # will eventually contain grouped anagrams
         for word in strs: # iterate through every string 
             if tuple(sorted(word)) in dic: # checks if it is already a KEY in dic
                 # if it is a key, means there's already 
                 # a group of anagrams for this set of characters
                 dic[tuple(sorted(word))].append(word) # appends current word to corresponding list of anagrams
             else:
-                dic[tuple(sorted(word))] = [word] 
+                dic[tuple(sorted(word))] = [word]
                 # if sorted tuple is not a key, it creates a New Key
                 # and a list containing current word as a VALUE
         for lists in dic.values(): # iterates through the vlues of dic
-            ans.append(word) # ans is a list, in which we append lists of values from dic
+            ans.append(lists) # ans is a list, in which we append lists of values from dic
         return ans
     
 
