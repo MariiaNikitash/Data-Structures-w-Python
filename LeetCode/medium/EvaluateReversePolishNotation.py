45ca4f58e18eb4986cce3bfd7c0ca59fec36e4e5
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2023-11-28 21:47:09 -0800

    used stack,and set operators, then if statements

--- LeetCode/medium/EvaluateReversePolishNotation.py
+++ LeetCode/medium/EvaluateReversePolishNotation.py
@@ -1,3 +1,20 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        
+        stack = []
+        operators = ('+', '-', '*', '/')
+        for index in tokens:
+            if index not in operators:
+                stack.append(index)
+                continue
+            a = int(stack.pop())
+            b = int(stack.pop())
+            if index == '+':
+                stack.append(int(a + b))
+            elif index == '*':
+                stack.append(int(a * b))
+            elif index == '-':
+                stack.append(int(b - a))
+            elif index == '/':
+                stack.append(int(b / a))
+        return int(stack[0])
+    
