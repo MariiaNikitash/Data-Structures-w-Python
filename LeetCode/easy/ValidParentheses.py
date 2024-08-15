b9847d6bbe01fde564125a07485632e96e40d947
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2024-08-14 19:06:25 -0700

    solved valid parentheses again and added worded solution

--- LeetCode/easy/ValidParentheses.py
+++ LeetCode/easy/ValidParentheses.py
@@ -14,4 +14,7 @@ class Solution:
         return len(stack) == 0
     
 
-    
+    # Solution: add all the opening brakets to the stack then when we encounter
+    # a closing braket we have to pop a last opening braket in the stack and compare if closing braket mathces opening braket in the dic
+    # if we start of with a closing braket, the stack will be empty so we return false immediately
+    # at the end we have to return an empty stack to ensure that no unmatches brakets remain
