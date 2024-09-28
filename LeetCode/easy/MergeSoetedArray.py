51234a94ca6a6bf6a724c7b70f3f5dfceb2c9aaa
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2024-09-27 22:15:12 -0700

    used 3 pointers to solve array problem

---
+++ LeetCode/easy/MergeSoetedArray.py
@@ -0,0 +1,16 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        ptr1 = m-1
+        ptr2 = n-1
+        last = m + n - 1
+        while n > 0:
+            if m > 0 and nums1[ptr1] > nums2[ptr2]:
+                nums1[last] = nums1[ptr1]
+                m -= 1 
+            else:
+                nums1[last] = nums2[n-1]
+                n -= 1 
+            last -= 1
