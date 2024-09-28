1798949f96cfb0852e241160a22490da65bd51ef
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2024-09-27 22:40:51 -0700

    solved the error in the problem

--- LeetCode/easy/MergeSoetedArray.py
+++ LeetCode/easy/MergeSoetedArray.py
@@ -6,11 +6,11 @@ class Solution:
         ptr1 = m-1
         ptr2 = n-1
         last = m + n - 1
-        while n > 0:
-            if m > 0 and nums1[ptr1] > nums2[ptr2]:
+        while ptr2 >= 0:
+            if ptr1 >= 0 and nums1[ptr1] > nums2[ptr2]:
                 nums1[last] = nums1[ptr1]
-                m -= 1 
+                ptr1 -= 1 
             else:
-                nums1[last] = nums2[n-1]
-                n -= 1 
+                nums1[last] = nums2[ptr2]
+                ptr2 -= 1 
             last -= 1
