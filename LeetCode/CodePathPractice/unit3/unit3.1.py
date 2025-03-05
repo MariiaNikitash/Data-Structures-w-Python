436e3566d55d0bcee7203deb758452bf321bcf0b
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2025-03-05 08:40:52 -0800

    str 3.1 done

--- LeetCode/CodePathPractice/unit3/unit3.1.py
+++ LeetCode/CodePathPractice/unit3/unit3.1.py
@@ -83,16 +83,21 @@ def min_distance(words, word1, word2):
         if str == word2:
             pos2 = i
     
-    
+    if pos1 is not None and pos2 is not None:
+            min_dist = min(min_dist, abs(pos1 - pos2))
+
+    return min_dist if min_dist != float('inf') else -1
+
+
 words = ["the", "quick", "brown", "fox", "jumped", "the"]
 dist1 = min_distance(words, "quick", "jumped")
 dist2 = min_distance(words, "the", "jumped")
 print(dist1)
-#print(dist2)
+print(dist2)
 
 words2 = ["code", "path", "code", "contribute",  "practice"]
 dist3 = min_distance(words2, "code", "practice")
-#print(dist3)
+print(dist3)
 
 
 
