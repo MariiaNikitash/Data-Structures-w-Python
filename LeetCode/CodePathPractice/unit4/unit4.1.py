a8412fc941ec6e6a8b9f4e0dc3d228ddccfb3a65
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2025-03-11 10:05:51 -0700

    reverse lis added

--- LeetCode/CodePathPractice/unit4/unit4.1.py
+++ LeetCode/CodePathPractice/unit4/unit4.1.py
@@ -10,6 +10,18 @@ def is_prime(n):
         i += 1
     return True
 
-print(is_prime(5))
-print(is_prime(12))
-print(is_prime(9))
+#print(is_prime(5))
+#print(is_prime(12))
+#print(is_prime(9))
+
+
+def reverse_list(lst):
+    r,l = 0, len(lst) -1
+    while r < l:
+        temp = lst[r]
+        lst[r] = lst[l]
+        lst[l] = temp
+        r +=1
+        l -=1
+    return lst
+print(reverse_list([1, 2, 3, 4, 5])) #[5, 4, 3, 2, 1]
