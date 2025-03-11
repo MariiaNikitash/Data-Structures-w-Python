5196b9c8d557731d9ed601b3af8091776f386d0c
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2025-03-11 09:49:16 -0700

    is prime using sqrt added

--- LeetCode/CodePathPractice/unit4/unit4.1.py
+++ LeetCode/CodePathPractice/unit4/unit4.1.py
@@ -1,7 +1,14 @@
 # 2 Pointers
-
+#A prime number is a number greater than 1 that has no positive divisors other than 1 and itself.
 def is_prime(n):
-    if n % 
+    if n <= 1:
+        return False
+    i = 2
+    while i * i <= n: #
+        if n % i == 0:
+            return False
+        i += 1
+    return True
 
 print(is_prime(5))
 print(is_prime(12))
