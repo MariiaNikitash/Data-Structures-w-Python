1dbe0245f0bfc87a598c1dc4f897258eb605db1f
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2025-03-25 10:16:30 -0700

    first LL promlem solved

--- LeetCode/CodePathPractice/unit5/unit5.2.py
+++ LeetCode/CodePathPractice/unit5/unit5.2.py
@@ -1,4 +1,28 @@
 # Linked Lists part 2
 
 
-# I did it 
+class Pokemon():
+	def  __init__(self, name, hp, damage):
+		self.name = name
+		self.hp = hp # hit points
+		self.damage = damage # The amount of damage this pokemon does to its opponent every attack
+#write a method attack() that takes in a Pokemon object opponent and decreases
+# opponent's hp by their self's damage amount.
+
+#If damaging the opponent leads to the opponent having an hp of 0 or less, the function should set the
+# opponent's hp to 0 and print out "<Opponent name> fainted>.
+#otherwise, the function should print out "<Pokemon name> dealt <damage> damage to <opponent name>"		
+	def attack(self, opponent):
+		opponent.hp -= self.damage
+		if opponent.hp <= 0:
+			opponent.hp = 0
+			print(f"{opponent.name} fainted")
+		else:
+			print(f"{self.name} dealt {self.damage} damage to {opponent.name}")
+	
+pikachu = Pokemon("Pikachu", 35, 20)
+bulbasaur = Pokemon("Bulbasaur", 45, 30) 
+
+opponent = bulbasaur
+pikachu.attack(opponent)
+
