472e407ea67816d468030f9d04b17dd13ced4426
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2025-04-01 23:22:59 -0700

    umnit6 started

--- LeetCode/CodePathPractice/unit6/unit6.1.py
+++ LeetCode/CodePathPractice/unit6/unit6.1.py
@@ -28,11 +28,48 @@ class Node:
 		self.next = next
 
 def count_element(head, val):
-	pass
+	count = 0
+	cur = head
+	while cur:
+		if cur.value == val:
+			count+=1
+			cur = cur.next
+	return count
 
 
 # Input List: 3 -> 1 -> 2 -> 1
 # Input: head = 3, val = 1
-
-
 # Output: 2
+
+# 3. Remove Tail
+class Node:
+    def __init__(self, value=None, next=None):
+        self.value = value
+        self.next = next
+        
+        
+# Helper function to print the linked list
+def print_list(node):
+    current = node
+    while current:
+        print(current.value, end=" -> " if current.next else "")
+        current = current.next
+    print()
+
+
+# I have a bug! 
+def remove_tail(head):
+    if head is None: # If the list is empty, return None
+        return None
+    if head.next is None: # If there's only one node, removing it leaves the list empty
+        return None 
+		
+	# Start from the head and find the second-to-last node
+    current = head
+    while current.next: 
+        current = current.next
+
+    current.next = None # Remove the last node by setting second-to-last node to None
+    return head
+
+#
