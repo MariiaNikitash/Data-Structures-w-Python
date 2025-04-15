e3a4651d16e6f64a9af3c92705117bd8328ab1c9
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2025-04-14 23:13:47 -0700

    rec started

--- LeetCode/CodePathPractice/unit7/unit7.1.py
+++ LeetCode/CodePathPractice/unit7/unit7.1.py
@@ -6,3 +6,18 @@ def repeat_hello(n):
 		repeat_hello(n - 1)
 		
 repeat_hello(5)
+
+
+def count_recursive(num):
+    # Action to repeat
+    print(f"Count {num}!")
+
+    # Base Case: If num is 1 we want to stop counting down
+    if num == 1:
+        # Terminate the function by returning
+        return
+
+    # Recursive Case: If num is larger than 1
+    else:
+       # Call count_recursive() again, but decrement the input value by 1
+       count_recursive(num - 1)
