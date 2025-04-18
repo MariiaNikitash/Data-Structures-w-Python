bddaaed5ed240bdfeebbeb9c833309acea8366d7
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2025-04-17 22:38:29 -0700

    rec did 2 problems

--- LeetCode/CodePathPractice/unit7/unit7.1.py
+++ LeetCode/CodePathPractice/unit7/unit7.1.py
@@ -6,20 +6,47 @@ def repeat_hello(n):
     
 		repeat_hello(n - 1)
 		
-repeat_hello(5)
+#repeat_hello(5)
 
-
-
-def count_recursive(num):
+# Problem: 2
+#Time Complexity: O(n) because we make n recursive calls.
+#Space Complexity: O(n) because each recursive call adds a layer to the call stack, using more memory.
+def factorial(num):
     # Action to repeat
-    print(f"Count {num}!")
-
-    # Base Case: If num is 1 we want to stop counting down
-    if num == 1:
-        # Terminate the function by returning
-        return
-
-    # Recursive Case: If num is larger than 1
+    print(f"Count {num}")
+    # Base case
+    if num == 0:
+        return 1
+    #Recursive case
     else:
-       # Call count_recursive() again, but decrement the input value by 1
-       count_recursive(num - 1)
+       return num * factorial(num-1)
+
+#print(factorial(5))
+
+# Iteravely it would take up O(1) space since no extra DS is used
+def fac(n):
+    res = 1
+    for i in range(1, n+1):
+        res *=i
+    return res
+#print(fac(5))
+
+
+# Problem 3: Recursive Sum
+def sum_list(lst):
+    if lst == []:
+        return 0
+    print(lst[0])
+    print(lst[1:])
+    return lst[0] + sum_list(lst[1:])
+
+print(sum_list([1, 2, 3, 4]))
+
+# The recursive calls stack up the additions, and they
+# don’t happen all at once. The function keeps calling itself until it
+# reaches the base case ([]), then starts adding the results back together.
+
+# Problem 4: Recursive Power of 2
+def is_power_of_two(n):
+	pass
+
