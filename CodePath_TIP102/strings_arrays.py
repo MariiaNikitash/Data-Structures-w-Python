02a053f80701a41e07c057013a34fe3d7fd8c603
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2025-06-07 23:34:54 -0700

    hacher2

--- CodePath_TIP102/strings_arrays.py
+++ CodePath_TIP102/strings_arrays.py
@@ -309,3 +309,16 @@ def find_needle(haystack, needle):
         if haystack[i: i+len(needle)] == needle:
             return i
     return -1
+
+# can place flowers
+# Sliding window 
+def canPlaceFlowers(flowerbed, n):
+    flowerbed = [0] + flowerbed + [0]
+    count = 0
+
+    for i in range(1, len(flowerbed) - 1):
+        if flowerbed[i-1] == flowerbed[i] == flowerbed[i+1] == 0:
+            flowerbed[i] = 1
+            count += 1
+
+    return count >= n
