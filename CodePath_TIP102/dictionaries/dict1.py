1132f0503d3b15f3141af2d573335445adf1ea45
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2025-06-14 23:33:45 -0700

    not optimized sol added Ow

--- CodePath_TIP102/dictionaries/dict1.py
+++ CodePath_TIP102/dictionaries/dict1.py
@@ -153,9 +153,3 @@ for code in codes:
 print(two_sum([2, 7, 8, 5], 7))
 
 
-### SESH 2
-1:2
-3:2
-2:3
-5:1
-7:1
--- CodePath_TIP102/dictionaries/sesh2.py
+++ CodePath_TIP102/dictionaries/sesh2.py
@@ -1,4 +1,35 @@
-#Problem 2: Verifying Authenticity
+#Problem 1: Balanced Art Collection
+#A balanced collection is one where the difference between the maximum and minimum value of the art pieces is exactly 1.
+#Ret len of longest subsequence 
+#Time: O(n)
+#Space: O(n)
+
+def find_balanced_subsequence(art_pieces):
+    dic = {}
+    for num in art_pieces:
+        if num in dic:
+            dic[num] +=1
+        else:
+            dic[num] =1
+    max_len = 0
+    for num in dic:
+        if num+1 in dic:
+            cur_len = dic[num] + dic[num + 1]
+            max_len = max(max_len, cur_len)
+    return max_len
+
+
+art_pieces1 = [1,3,2,2,5,2,3,7] # Output: 5  so [3,2,2,2,3] is the longest subsequence of adj nums
+art_pieces2 = [1,2,3,4] # 2
+art_pieces3 = [1,1,1,1] # 0
+
+#print(find_balanced_subsequence(art_pieces1))
+#print(find_balanced_subsequence(art_pieces2))
+#print(find_balanced_subsequence(art_pieces3))
+
+
+#----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*
+# #Problem 2: Verifying Authenticity
 # The base[n] array is defined as [1, 2, ..., n - 1, n, n], meaning it 
 # array of length n + 1 containing the integers from 1 to n - 1 exactly once, and int n twice. Ex: base[1] is [1, 1] and base[3] is [1, 2, 3, 3].
 
@@ -28,6 +59,39 @@ collection1 = [2, 1, 3] # F
 collection2 = [1, 3, 3, 2] # T
 collection3 = [1, 1] # T
 
-print(is_authentic_collection(collection1))
-print(is_authentic_collection(collection2))
-print(is_authentic_collection(collection3))
+#print(is_authentic_collection(collection1))
+#print(is_authentic_collection(collection2))
+#print(is_authentic_collection(collection3))
+
+
+def organize_exhibition(collection):
+    result = []
+    counts = dict()
+    for item in collection:
+        if item in counts:
+            counts[item] += 1
+        else:
+            counts[item] = 1
+
+    maxx = 0
+    for item in counts.keys():
+        maxx = max(counts[item], maxx)
+
+    for i in range(maxx):
+        result.append(set())
+
+    for item in counts.keys(): 
+        for i in range(counts[item]): 
+            result[i].add(item)
+
+    for i in range(len(result)):
+        result[i] = list(result[i])
+    return result
+
+collection1 = ["O'Keefe", "Kahlo", "Picasso", "O'Keefe", "Warhol", 
+              "Kahlo", "O'Keefe"]
+collection2 = ["Kusama", "Monet", "Ofili", "Banksy"]
+
+print(organize_exhibition(collection1))
+print(organize_exhibition(collection2))
+
