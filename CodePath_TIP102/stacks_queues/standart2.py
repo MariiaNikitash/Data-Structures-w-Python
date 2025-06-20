d05b2ef32dea825ea3328116186eaaac5186d648
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2025-06-20 08:30:53 -0700

    2 ptr done

--- CodePath_TIP102/stacks_queues/standart2.py
+++ CodePath_TIP102/stacks_queues/standart2.py
@@ -8,11 +8,35 @@ def final_supply_costs(costs):
             res[j] -= costs[i]
         stack.append(i)
     return res
-print(final_supply_costs([8, 4, 6, 2, 3])) 
+#print(final_supply_costs([8, 4, 6, 2, 3])) 
 #print(final_supply_costs([1, 2, 3, 4, 5])) 
 #print(final_supply_costs([10, 1, 1, 6])) 
 
+#----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*
+# Problem 2: Find First Symmetrical Landmark Name
 
-[4, 2, 4, 2, 3]
-[1, 2, 3, 4, 5]
-[9, 0, 1, 6]
+def first_symmetrical_landmark(landmarks):
+    res = ''
+    def palindrome(word):
+        l,r = 0, len(word)-1
+        while l < r:
+            if word[l] != word[r]:
+                return False
+            l+=1
+            r-=1
+        return True
+    
+    for mark in landmarks:
+        if (palindrome(mark)):
+            res += mark
+            return res
+    return ''
+    
+    
+
+
+
+print(first_symmetrical_landmark(["canyon","forest","rotor","mountain", "ama"])) 
+print(first_symmetrical_landmark(["plateau","valley","cliff"])) 
+
+#rotor
