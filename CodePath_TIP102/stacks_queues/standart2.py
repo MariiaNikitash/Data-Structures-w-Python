05fad7e1ca4b68a1524ec6befdf83532d24995e6
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2025-06-20 17:58:41 -0700

    2 ptr done again

--- CodePath_TIP102/stacks_queues/standart2.py
+++ CodePath_TIP102/stacks_queues/standart2.py
@@ -8,9 +8,9 @@ def final_supply_costs(costs):
             res[j] -= costs[i]
         stack.append(i)
     return res
-#print(final_supply_costs([8, 4, 6, 2, 3])) 
-#print(final_supply_costs([1, 2, 3, 4, 5])) 
-#print(final_supply_costs([10, 1, 1, 6])) 
+#print(final_supply_costs([8, 4, 6, 2, 3])) [4,2,4,2,3]
+#print(final_supply_costs([1, 2, 3, 4, 5]))  [1, 2, 3, 4, 5]
+#print(final_supply_costs([10, 1, 1, 6]))  [9, 0, 1, 6]
 
 #----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*
 # Problem 2: Find First Symmetrical Landmark Name
@@ -35,6 +35,7 @@ def first_symmetrical_landmark(landmarks):
 #print(first_symmetrical_landmark(["canyon","forest","rotor","mountain", "ama"])) rotor
 #print(first_symmetrical_landmark(["plateau","valley","cliff"])) " "
 
+#----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*
 # Problem 3: Terrain Elevation Match
 def terrain_elevation_match(terrain):
     low,high = 0, len(terrain)
@@ -55,10 +56,34 @@ def terrain_elevation_match(terrain):
     #    res.append(high)
     return res
 
-print(terrain_elevation_match("IDID")) #[0, 4, 1, 3, 2]
-print(terrain_elevation_match("III")) #[0, 1, 2, 3]
-print(terrain_elevation_match("DDI")) #[3, 2, 0, 1]
+#print(terrain_elevation_match("IDID")) #[0, 4, 1, 3, 2]
+#print(terrain_elevation_match("III")) #[0, 1, 2, 3]
+#print(terrain_elevation_match("DDI")) #[3, 2, 0, 1]
 
+#----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*
+# Problem 4:
+
+
+def find_the_log_conc_val(logs):
+    if not logs:
+        return 0
+    if len(logs) == 1:
+        return logs[0]
+    l,r = 0, len(logs)-1
+    res = 0
+    while l<=r:
+        if l == r:
+            res += logs[l]
+        else:
+            concat = (str(logs[l]) + str(logs[r]))
+            res += int(concat)
+        l+=1
+        r-=1
+
+    return res
+
+print(find_the_log_conc_val([7, 52, 2, 4])) #596
+print(find_the_log_conc_val([5, 14, 13, 8, 12])) #673
 
 
 
