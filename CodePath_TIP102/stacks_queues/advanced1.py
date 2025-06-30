ae15adb3d7249c26b82b08f5310e40666b022b91
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2025-06-30 12:57:44 -0700

    dutch flag problem solved

--- CodePath_TIP102/stacks_queues/advanced1.py
+++ CodePath_TIP102/stacks_queues/advanced1.py
@@ -43,11 +43,44 @@ def deckRevealedIncreasing(deck):
       if q:
          q.append(q.popleft())
    return res
-print(deckRevealedIncreasing([17,13,11,2,3,5,7])) 
-print(deckRevealedIncreasing([1,1000])) 
+#print(deckRevealedIncreasing([17,13,11,2,3,5,7])) 
+#print(deckRevealedIncreasing([1,1000])) 
 
 #----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*
 # Problem 3 
+# Arrange Event Attendees by Priority. The relative order of the attendees within each priority group (less than, equal to, greater than) must be preserved.
+def arrange_attendees_by_priority(attendees, priority):
+   less = []
+   equal = []
+   greater = []
+   for a in attendees:
+      if a < priority:
+         less.append(a)
+      elif a > priority:
+         greater.append(a)
+      else:
+         equal.append(a)
+   return less + equal + greater
+
+
+print(arrange_attendees_by_priority([9,12,5,10,14,3,10], 10)) # [9,5,3,10,10,12,14]
+# Or i can do a Dutch National Flag algorithm, but the order will not be preserved, i would use 3 pointers to partition into 3 groups and swap elements 
+# Sort Colors: Leetcode Medium
+def dutch_national_flag(nums):
+    low, mid, high = 0, 0, len(nums) - 1
+
+    while mid <= high:
+        if nums[mid] == 0:
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low += 1
+            mid += 1
+        elif nums[mid] == 1:
+            mid += 1
+        else:  # nums[mid] == 2
+            nums[mid], nums[high] = nums[high], nums[mid]
+            high -= 1
+#Input: nums = [2,0,2,1,1,0]
+#Output: [0,0,1,1,2,2]
 
 #----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*
 # Problem 4 
